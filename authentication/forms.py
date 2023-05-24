6d168fb46e34d6a0b93e7449730f705e369eaa6e
Shubhangi-Ranjan-05 <88038562+Shubhangi-Ranjan-05@users.noreply.github.com>
2023-05-24 16:02:26 +0530

    Update forms.py

--- authentication/forms.py
+++ authentication/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 # importing default user registration form
 from django.contrib.auth.forms import UserCreationForm
 # importing built-in user model
-from django.contrib.auth.forms import User
+from django.contrib.auth.models import User
 from django import forms
 from ads.models import Author
 
